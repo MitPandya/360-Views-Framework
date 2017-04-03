92a3e76f58e4ffc82b322def1a7cef3169a7f9cd
mitkumar <mhpandya@ncsu.edu>
2017-04-03 13:43:04 -0400

    deploying

--- LibraryProjects/settings.py
+++ LibraryProjects/settings.py
@@ -81,6 +81,9 @@ DATABASES = {
     }
 }
 
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
