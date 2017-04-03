3dbf1021f1f868093126ff39dd2ccbc7bcd102a8
mitkumar <mhpandya@ncsu.edu>
2017-04-03 13:32:43 -0400

    deploying

--- LibraryProjects/settings.py
+++ LibraryProjects/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    '_360_Views_Framework',
+    '360_Views_Framework',
 ]
 
 MIDDLEWARE = [
--- LibraryProjects/urls.py
+++ LibraryProjects/urls.py
@@ -18,7 +18,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'', include('_360_Views_Framework.urls')),
+    url(r'', include('360_Views_Framework.urls')),
     url(r'^admin/', admin.site.urls),
 ]
 
