fee80cb3560bd03f4cc3ad7d385ba3a7c1479398
mitkumar <mhpandya@ncsu.edu>
2017-04-03 13:07:05 -0400

    deploying to heroku

---
+++ LibraryProjects/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for _360_views_frameork project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LibraryProjects.settings")
+
+application = get_wsgi_application()
