4b561c075019528c343effd98ac86879b7ed086f
mitkumar <mhpandya@ncsu.edu>
2017-04-04 11:32:26 -0400

    deploying

--- LibraryProjects/settings.py
+++ LibraryProjects/settings.py
@@ -120,10 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = './Framework/static/'
+STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static1'),
+    os.path.join(PROJECT_ROOT, './Framework/static'),
 )
 
 # Simplified static file serving.
