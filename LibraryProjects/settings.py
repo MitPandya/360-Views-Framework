46b8f733d3df744284faffed44979ce381d55974
mitkumar <mhpandya@ncsu.edu>
2017-05-04 12:48:33 -0400

    Implemented heroku database integration

--- LibraryProjects/settings.py
+++ LibraryProjects/settings.py
@@ -78,10 +78,10 @@ WSGI_APPLICATION = 'LibraryProjects.wsgi.application'
 
 DATABASES = {
 
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
 
 }
 
