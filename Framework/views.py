d83a877b91efa0ec0be4e00626ca6314a8963ad3
mitkumar <mhpandya@ncsu.edu>
2017-05-15 14:37:59 -0400

    deploying to heroku

--- Framework/views.py
+++ Framework/views.py
@@ -3,12 +3,17 @@ from django.shortcuts import render
 import json
 from django.views.decorators.csrf import csrf_exempt
 from Framework import imageController
+from django.views.decorators.clickjacking import xframe_options_exempt
 #from _360_Views_Framework import imageController
 from django.http import request, response
 
 # Create your views here.6
 
 from .models import Image
+
+@xframe_options_exempt
+def ok_to_load_in_a_frame(request):
+    return HttpResponse("This page is safe to load in a frame on any site.")
 
 def index(request):
     return render(request,"index.html",{})
